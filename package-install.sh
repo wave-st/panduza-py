e214c08d9a2830f1b4be43575338932f3ee1202c
xdoctorwhoz <xavier.rodriguezalvarez@gmail.com>
2022-05-13 14:04:31 +0200

    Update install scripts

--- package-install.sh
+++ package-install.sh
@@ -2,6 +2,3 @@ tar_filepath=`readlink -f ./dist/panduza*.tar.gz`
 echo "Update from : $tar_filepath"
 python3 setup.py sdist bdist_wheel
 pip install --upgrade --ignore-installed $tar_filepath
-
-
-# pip list | grep pza
